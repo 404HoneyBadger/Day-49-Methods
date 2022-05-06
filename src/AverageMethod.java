4e72c293096fbf5328d086a313951e9d22d13107
404HoneyBadger <94186840+404HoneyBadger@users.noreply.github.com>
2022-05-06 07:53:57 -0400

    second

--- src/AverageMethod.java
+++ src/AverageMethod.java
@@ -1,25 +1,30 @@
-
+import java.util.Scanner;
 public class AverageMethod {
-    public static void main(String[] args) {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter the first number:");
+        double numOne = Double.valueOf(scanner.nextLine());
         
-        System.out.println("The average is: " + Integer.average(1,2,4));
-    
+        System.out.println("Enter the next number:");
+        double numTwo = Double.valueOf(scanner.nextLine());
+        
+        System.out.println("Enter the last number:");
+        double numThree = Double.valueOf(scanner.nextLine());
+
+        double sum = sumThree(numOne, numTwo, numThree);
+        double average = averageThree(sum);
+        
+        System.out.println("The average is : " + average);
     }
 
-    private static int sum(int a, int b, int c) {
-    
-        return( a+b+c);
+    public static double sumThree(double num1, double num2, double num3){
+        double sum = num1 + num2 + num3;
+        return sum;
     }
 
-    public static int average() {
-        return(sum/3);
-
+    public static double averageThree(double sum){
+        double average = sum/3;
+        return average;
     }
-
-
-
-    
 }
-    
-
